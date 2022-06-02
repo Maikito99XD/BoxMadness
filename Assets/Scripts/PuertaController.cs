f18e4f0a1070fc9b70f1b8268a455c8ab966454b
Maikito99XD <msteelmarin@gmail.com>
2022-06-02 20:28:09 +0200

    update 02/05

--- Assets/Scripts/PuertaController.cs
+++ Assets/Scripts/PuertaController.cs
@@ -5,25 +5,13 @@ using UnityEngine.Playables;
 
 public class PuertaController : MonoBehaviour
 {
-    private CombateJugador combateJugador;
-    private PlayableDirector playable;
-    private bool validado = false;
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            combateJugador = collision.GetComponent<CombateJugador>();
-            Debug.Log("He llegado");
-            if (combateJugador.llavesJugador > 0 )
-            {
-                Debug.Log("He entrado");
-              
-                if (validado)
-                {
-                    AnimacionPuerta();
-                }
-            }
+            
+          
         }
     }
     public void AnimacionPuerta()
